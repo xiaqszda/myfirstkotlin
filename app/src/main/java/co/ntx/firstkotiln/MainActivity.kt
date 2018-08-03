9577910d7a31ad79a46a81174f8568bfaeeed288
Xtn <xtnaj@qq.com>
2018-08-03 10:43:19 +0800

    测试kotlin

--- app/src/main/java/co/ntx/firstkotiln/MainActivity.kt
+++ app/src/main/java/co/ntx/firstkotiln/MainActivity.kt
@@ -2,12 +2,23 @@ package co.ntx.firstkotiln
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
+//    var tv : TextView = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_main)
+        val tv = findViewById<TextView>(R.id.text1)
+        tv.text = ("hello world!")
+    }
+
+    private fun init(){
+
     }
 
     fun a(a : Int, b : Int) : Int{
